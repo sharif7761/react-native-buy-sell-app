2b340c585d7f51d96005c4721f153d4671c4d7bd
Puspita Vabna <sharif7761@gmail.com>
2021-10-04 23:02:17 +0600

    form components

---
+++ app/components/forms/SubmitButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {useFormikContext} from "formik";
+
+import AppButton from "../AppButton";
+
+const SubmitButton = ({title}) => {
+    const {handleSubmit} = useFormikContext();
+    return (
+        <AppButton
+            title={title}
+            onPress={handleSubmit}
+        />
+    );
+};
+
+export default SubmitButton;
