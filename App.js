5796321ebe9f179d67f1587ce71246e7f5f85efa
Puspita Vabna <sharif7761@gmail.com>
2021-10-08 20:48:47 +0600

    installed neccessary packages

--- App.js
+++ App.js
@@ -21,6 +21,12 @@ import ImageInputList from "./app/components/ImageInputList";
 import AppText from "./app/components/AppText";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AuthNavigator from "./app/navigation/AuthNavigator";
+import navigationTheme from "./app/navigation/navigationTheme";
+import AppNavigator from "./app/navigation/AppNavigator";
+import OfflineNotice from "./app/components/OfflineNotice";
 
 export default function App() {
 
@@ -30,7 +36,7 @@ export default function App() {
         return(
         <Button
             title="View Tweet"
-            onPress={() => navigation.navigate("TweetDetails")}
+            onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
         />
     )};
 
@@ -45,24 +51,67 @@ export default function App() {
         </Screen>
     );
 
-    const TweetDetails = () => (
+    const TweetDetails = ({ route }) => (
         <Screen>
-            <AppText>Tweet Details</AppText>
+            <AppText>Tweet Details {route.params.id} </AppText>
+        </Screen>
+    );
+
+    const Account = () => (
+        <Screen>
+            <AppText>Account </AppText>
         </Screen>
     );
 
     const Stack = createStackNavigator();
     const StackNavigator = () => (
-        <Stack.Navigator>
-            <Stack.Screen name="Tweets" component={Tweets} />
-            <Stack.Screen name="TweetDetails" component={TweetDetails} />
+        <Stack.Navigator
+            screenOptions={{
+                headerStyle: { backgroundColor: 'blue' },
+            }}
+        >
+            <Stack.Screen
+                name="Tweets"
+                component={Tweets}
+                options={{
+                    headerStyle: { backgroundColor: 'tomato' },
+                }}
+            />
+            <Stack.Screen
+                name="TweetDetails"
+                component={TweetDetails}
+                options={({ route }) => ({title: route.params.id })}
+            />
         </Stack.Navigator>
     );
 
+    const Tab = createBottomTabNavigator();
+    const TabNavigator = () => (
+        <Tab.Navigator
+            tabBarOptions={{
+                activeBackgroundColor: "tomato",
+                inctiveBackgroundColor: "#eee",
+                activeTintColor: "white",
+                inctiveTintColor: "black",
+            }}
+        >
+            <Tab.Screen
+                name="Feed"
+                component={Tweets}
+                options={{
+                    tabBarIcon: ({ size, color }) => <MaterialCommunityIcons name='home' size={size} color={color} />
+                }}
+            />
+        </Tab.Navigator>
+    );
+
   return (
-        <NavigationContainer>
-            <StackNavigator />
-        </NavigationContainer>
+        <>
+            <NavigationContainer theme={navigationTheme} >
+                <AppNavigator />
+            </NavigationContainer>
+            <OfflineNotice />
+        </>
       // <Screen>
       //     <ImageInputList
       //         imageUris={imageUris}
@@ -70,7 +119,7 @@ export default function App() {
       //         onRemoveImage={uri => handleRemove(uri)}
       //     />
       // </Screen>
-     //<WelcomeScreen/>
+    // <WelcomeScreen/>
      //  <ViewImageScreen/>
     //   <View style={{
     //     backgroundColor: '#f8f4f4',
