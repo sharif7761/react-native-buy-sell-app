3cced219d259811f0960fd3612a2b4bd42eff064
Puspita Vabna <sharif7761@gmail.com>
2021-10-10 00:47:02 +0600

    account routes

---
+++ app/navigation/AccountNavigator.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createStackNavigator } from "@react-navigation/stack";
+import AccountScreen from "../screens/AccountScreen";
+import MessageScreen from "../screens/MessageScreen";
+import routes from "./routes";
+
+
+
+const Stack = createStackNavigator();
+const AccountNavigator = () => (
+    <Stack.Navigator>
+        <Stack.Screen
+            name={routes.ACCOUNT}
+            component={AccountScreen}
+        />
+        <Stack.Screen
+            name={routes.MESSAGES}
+            component={MessageScreen}
+        />
+    </Stack.Navigator>
+);
+
+export default AccountNavigator;
