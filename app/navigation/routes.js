b3c916bf2d7d831a641d26588bfbabe8da647a8b
Puspita Vabna <sharif7761@gmail.com>
2021-10-09 23:20:52 +0600

    routes

---
+++ app/navigation/routes.js
@@ -0,0 +1,8 @@
+export default Object.freeze({
+    ACCOUNT: 'Account',
+    FEED: 'Feed',
+    LISTINGS:'Listings',
+    LISTING_DETAILS: 'listingDetails',
+    LISTING_EDIT: 'ListingEdit',
+    MESSAGES: 'Messages'
+});
