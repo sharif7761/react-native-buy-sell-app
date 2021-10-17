14e5b2ae8eacb071f8337dcfc0bf064f2af2c079
Puspita Vabna <sharif7761@gmail.com>
2021-10-17 22:58:19 +0600

    custom form field

--- app/components/forms/AppFormField.js
+++ app/components/forms/AppFormField.js
@@ -6,13 +6,14 @@ import ErrorMessage from "./ErrorMessage";
 
 const AppFormField = ({name, width, ...otherProps}) => {
 
-    const {setFieldTouched, handleChange, errors, touched} = useFormikContext();
+    const {setFieldTouched, setFieldValue, values, errors, touched} = useFormikContext();
 
     return (
         <>
             <AppTextInput
                 onBlur={()=>setFieldTouched(name)}
-                onChangeText={handleChange(name)}
+                onChangeText={text => setFieldValue(name, text) }
+                value={values[name]}
                 width={width}
                 {...otherProps}
              />
